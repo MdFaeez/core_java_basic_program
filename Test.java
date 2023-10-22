a51945c0efc8ee2fc2a0296e7f217c4d071a6468
Mohd Faeez <faeezrehman789@gmail.com>
2023-10-22 14:15:35 +0530

    add new method for addition two number

--- Test.java
+++ Test.java
@@ -1,8 +1,14 @@
 class Test{
 
+public static void m1(int a,int b){
+
+int sum=a+b;
+System.out.println(sum);
+}
 public static void main(String args[]){
 
 System.out.println("Hello");
+m1(20,30);
 
 }
 }
